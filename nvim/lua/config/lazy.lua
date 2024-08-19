480836cf2fcf1bc63cac5369444df433bffab03d
yaroslav <20459871+dusaniuk@users.noreply.github.com>
2024-08-19 13:33:11 +0300

    feat: add lsp server

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { import = "config.plugins" },
+  { import = "config.plugins.lsp" },
 }, {
   checker = {
     enabled = true,
@@ -23,3 +24,4 @@ require("lazy").setup({
     notify = false,
   },
 })
+
